2a6e3374fbe640fcf7d3f246b9e0cb9a2070f09b
ZAIJIANLUOYE110 <779793115@qq.com>
2017-02-17 13:18:38 +0800

    Initial Commit

---
+++ hello-react/app/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class HelloComponent extends React.Component {
+    render () {
+        return  (<h1>Hello beauty!</h1>);
+    }
+}
---
+++ hello-react/webpack.config.js
@@ -0,0 +1,16 @@
+var webpack = require('webpack');
+module.exports = {
+    entry: ['./app/main.js'],
+    output: {
+        path: __dirname + '/dist',
+        filename: './bundle.js'
+    },
+    module: {
+        loaders: [
+          {
+            test: /\.jsx?$/,
+            loader: 'babel-loader'
+          }
+        ]
+    }
+}
