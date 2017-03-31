8ab3ebfeed4d39397b85ac60f92910908686d1f5
ZAIJIANLUOYE110 <779793115@qq.com>
2017-03-31 15:53:29 +0800

    增加用户编辑功能

---
+++ zhi-hu/src/components/Users/UsersEdit.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { Modal, Form, Input } from 'antd';
+
+class UsersEdit extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visible: false,
+    };
+  }
+
+  showModel(e) {
+    if (e) e.stopPropagation();
+    this.setState({
+      visible: true,
+    });
+  }
+
+  hideModel() {
+    this.setState({
+      visible: false,
+    });
+  }
+
+  okHandler() {
+    const { onOk } = this.props;
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        onOk(values);
+        this.hideModel();
+      }
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+    const { getFieldDecorator } = this.props.form;
+    const { name, email, website } = this.props.record;
+    const formItemLayout = {
+      labelCol: { span: 6 },
+      wrapperCol: { span: 14 },
+    };
+
+    return (
+      <span>
+        <span onClick={this.showModel.bind(this)}>
+          { children }
+        </span>
+        <Modal
+          title="Edit User"
+          visible={this.state.visible}
+          onOk={this.okHandler}
+          onCancel={this.hideModel.bind(this)}
+        >
+          <Form horizontal onSubmit={this.okHandler}>
+            <Form.Item
+              {...formItemLayout}
+              label="Name"
+            >
+              {
+                getFieldDecorator('name', {
+                  initialValue: name,
+                })(<Input />)
+              }
+            </Form.Item>
+            <Form.Item
+              {...formItemLayout}
+              label="Email"
+            >
+              {
+                getFieldDecorator('email', {
+                  initialValue: email,
+                })(<Input />)
+              }
+            </Form.Item>
+            <Form.Item
+              {...formItemLayout}
+              label="Website"
+            >
+              {
+                getFieldDecorator('website', {
+                  initialValue: website,
+                })(<Input />)
+              }
+            </Form.Item>
+          </Form>
+        </Modal>
+      </span>
+    );
+  }
+}
+
+export default Form.create()(UsersEdit);
--- zhi-hu/src/models/users.js
+++ zhi-hu/src/models/users.js
@@ -28,6 +28,16 @@ export default {
       const page = yield select(state => state.users.page);
       yield put({ type: 'fetch', payload: { page } });
     },
+    * patch({ payload: { id, values } }, { call, put, select }) {
+      yield call(usersService.patch, id, values);
+      const page = yield select(state => state.users.page);
+      yield put({ type: 'fetch', payload: { page } });
+    },
+    * dispatchEvent({ payload: { id, values } }, { call, put, select }) {
+      yield call(usersService.patch, id, values);
+      const page = yield select(state => state.users.page);
+      yield put({ type: 'fetch', payload: { page } });
+    },
   },
   subscriptions: {
     setup({ dispatch, history }) {
